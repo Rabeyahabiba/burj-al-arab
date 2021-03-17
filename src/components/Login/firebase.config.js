8341696c002b8624d0d005aebfc78865334499b1
Rabeyahabiba <hema.rabeya93@gmail.com>
2021-03-17 21:39:23 +0600

    added

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBv6y_IZy23CP3AvAle9e7ifATJI3n3FYs",
+    authDomain: "burj-f0958.firebaseapp.com",
+    projectId: "burj-f0958",
+    storageBucket: "burj-f0958.appspot.com",
+    messagingSenderId: "855792363507",
+    appId: "1:855792363507:web:48d58eea43d73ffe6c1486"
+  };
+  export default firebaseConfig;
